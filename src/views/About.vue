d911f2d77abbb2a6b2dcf2abd4579e87be53ff08
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-14 15:51:08 +0300

    update about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
     <h1>À propos de Kamusy</h1>
     <p>
       Kamusy signifie dictionnaire en mahorais et en swahili. L'objectif de ce
-      projet est de contribuer au développement des langues régionales de
-      Mayotte en fournissant une lexique en ligne.
+      projet est de contribuer à la diffusion des langues régionales de Mayotte
+      en fournissant une lexique en ligne.
     </p>
     <p>
       L'application est toujours en cours de développement. De nouvelles
