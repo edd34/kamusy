f7e13be9fc468bc1ab0c2ebff60d706c21da5e99
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-25 20:05:36 +0300

    update login component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+    <v-banner two-line transition="slide-y-transition" vif v-if="is_connected">
+      Vous êtes connecté.
+      <template v-slot:actions="{ dismiss }">
+        <v-btn text color="primary" @click="dismiss"> Effacer </v-btn>
+      </template>
+    </v-banner>
     <v-container>
       <form>
         <v-text-field
           v-model="name"
           counter
           label="Nom d'utilisateur"
+          :disabled="is_connected"
           required
         ></v-text-field>
         <v-text-field
@@ -16,27 +23,58 @@
           name="input-10-1"
           label="Mot de passe"
           counter
+          :disabled="is_connected"
           @click:append="show1 = !show1"
         ></v-text-field>
-        <v-btn class="mr-4" @click="submit"> Valider </v-btn>
-        <v-btn @click="clear"> Effacer </v-btn>
+        <v-btn class="mr-4" @click="submit" :disabled="is_connected">
+          Valider
+        </v-btn>
+        <v-btn @click="clear" :disabled="is_connected"> Effacer </v-btn>
       </form>
     </v-container>
+    <v-snackbar v-model="snackbar">
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   data: () => ({
     name: "",
     show1: false,
     password: "",
+    snackbar: false,
+    text: "",
   }),
+  computed: {
+    ...mapGetters({
+      is_connected: "store_account/is_connected",
+    }),
+  },
   methods: {
-    submit() {
-      this.$store.dispatch("store_account/login_query_token", {
-        username: this.name,
-        password: this.password,
-      });
+    async submit() {
+      const res = await this.$store.dispatch(
+        "store_account/login_query_token",
+        {
+          username: this.name,
+          password: this.password,
+        }
+      );
+      this.snackbar = true;
+      if (res) {
+        this.text = "Connexion réussie.";
+        this.clear();
+      } else {
+        this.text = "Identifiant ou mot de passe incorrect.";
+      }
     },
     clear() {
       this.name = "";
