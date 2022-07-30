f74bfef8b673a824b62a187cc4608a7dbdb94ed6
edd34 <eddine.farid@hotmail.fr>
2022-07-30 09:59:45 +0300

    update project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '^/': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/': ''
+        },
+      },
+    }
+  }
 }
