6880e7cef3661966a0e73f6f96607e9ab9fe7c4e
edd34 <eddine.farid@hotmail.fr>
2022-08-02 20:28:46 +0300

    update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="about pa-6">
-    <h1>À propos de Kamusy</h1>
+    <h1>À propos</h1>
+    <h2>Kamusy</h2>
     <p>
       Kamusy signifie dictionnaire en mahorais et en swahili. L'objectif de ce
       projet est de contribuer à la diffusion des langues régionales de Mayotte
-      en fournissant une lexique en ligne.
+      en fournissant un lexique interactif en ligne.
+    </p>
+    <h2>MAE Solutions</h2>
+    <p>
+      MAE Solutions est une entreprise de service numérique fondée à Mayotte en
+      2021 par des jeunes mahorais souhaitant apporter leur part de
+      développement à l'essor du territoire.
     </p>
     <p>
-      L'application est toujours en cours de développement. De nouvelles
-      fonctionnalités sont à venir.
+      Notre objectif est de permettre aux entreprises mahoraises de tirer parti
+      du numérique pour améliorer les process, augmenter le CA et réduire les
+      coûts grâce à une véritable ingénierie informatique.
     </p>
-    <p>
-      N'hésitez pas à envoyer un mail pour me faire part de vos remarques à
-      <a
-        href="mailto:eddine.farid@hotmail.fr?subject=À propos de Kamusy"
-        >eddine.farid@hotmail.fr</a
-      >
+    <p>Nous avons deux principales offres :</p>
+
+    <ul>
+      <li>
+        <b> Consulting : </b>
+        nous partons des besoins de l'entreprise entreprise et livrons une
+        solution qui répond à votre problématique métier. Dites nous juste ce
+        qui vous tracasse dans votre business, vous serez surpris par ce que
+        nous allons vous proposez.
+      </li>
+      <li>
+        <b> Site internet et webmarketing : </b>
+        afin de vous rendre visible sur internet, nous proposons la création de
+        site internet (vitrine, ecommerce...), logo, affiche, page Google
+        Maps... Tout ce dont vous avez besoin pour rayonner et conquérir de
+        nouveaux marchés.
+      </li>
+    </ul>
+    <br>
+    <p>N'hésitez pas à nous contacter et nous suivre !
+    <ul>
+      <li>
+        <a href="mailto:contact@mae-solutions.yt?subject=À propos de Kamusy"
+          >contact@mae-solutions.yt</a
+        >
+      </li>
+      <li>
+        <a href="https://www.mae-solutions.yt">https://www.mae-solutions.yt</a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/maesolutions976">https://www.facebook.com/maesolutions976</a>
+      </li>
+    </ul>
     </p>
   </div>
 </template>
